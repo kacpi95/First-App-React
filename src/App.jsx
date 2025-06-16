5e291b45a4909dc62e8807deeadf89aa0e7df534
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-06-16 13:47:34 +0200

    Add comonent Home and refactor App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import Hero from './components/Hero/Hero';
-import SearchForm from './components/SearchForm/SearchForm';
-import List from './components/List/List';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Favorite from './components/Favorite/Favorite';
 
 function App() {
   return (
     <>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Home />
+        <About />
+        <Favorite />
       </Container>
     </>
   );
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from '../Hero/Hero';
+import SearchForm from '../SearchForm/SearchForm';
+import List from '../List/List';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <SearchForm />
+      <List />
+    </>
+  );
+}
